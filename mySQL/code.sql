01428613d173f8216b443fe07e116627e555c2ba
PleatherShaman <shamanwebsolutions@gmail.com>
2019-09-21 16:44:18 +1000

    add routes to register new user

--- mySQL/code.sql
+++ mySQL/code.sql
@@ -5,3 +5,19 @@ USE mailing_list;
 SELECT DATABASE();
 
 
+CREATE TABLE users (
+    email  VARCHAR(255) PRIMARY KEY,
+    created_at TIMESTAMP DEFAULT NOW()
+);
+
+DESC users;
+
+INSERT INTO users (email)
+VALUES 
+    ('john@gmail.com'),
+    ("peter@gmail.com");
+
+SELECT * FROM users;
+
+
+
--- server.js
+++ server.js
@@ -1,37 +1,43 @@
-require('dotenv').config()
-const express = require('express');
-const  mysql = require('mysql');
-const faker = require('faker');
-
+require("dotenv").config();
+const express = require("express");
+const mysql = require("mysql");
+const faker = require("faker");
 
 const app = express();
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 var connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',    
-    password : process.env.MYSQL_PASSWORD,
-    database : 'mailing_list'
+  host: "localhost",
+  user: "root",
+  password: process.env.MYSQL_PASSWORD,
+  database: "mailing_list"
+});
+
+
+app.get("/", (req, res) => {
+  const q = "SELECT COUNT(*) as count FROM users";
+  connection.query(q, (error, results) => {
+    if (error) throw error;
+    const msg = `We have ${results[0].count} users`;
+    res.send(msg);
   });
+});
 
-  const q = 'SELECT CURDATE()'
+app.post("/register", (req, res) => {
+  const newUser = { email: req.body.email };
+  connection.query("INSERT INTO users SET ?", newUser, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.statusCode(500);
+    }
+    console.log(result);
+    res.redirect("/");
+  });
+});
 
-  connection.query(q, (error, results, fields)=>{
-      if(error){
-          console.log(error)
-      };
-      console.log (`${results}`)
-      console.log (`${fields}`)
-  })
+const PORT = process.env.PORT || 5000;
 
-
-  console.log(faker.internet.email())
-
-const PORT = process.env.PORT || 5000 ;
-
-
-app.listen(PORT, ()=>{
-    console.log(`Server started on port ${PORT}`)
-})
-
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
